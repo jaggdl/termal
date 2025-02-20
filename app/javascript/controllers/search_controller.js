982d33b43c519a6439ac560717f6c174f701cfc6
jaggdl <ja125garcia@gmail.com>
2025-02-20 01:08:52 -0600

    Removes timeouts from search

--- app/javascript/controllers/search_controller.js
+++ app/javascript/controllers/search_controller.js
@@ -4,36 +4,22 @@ export default class extends Controller {
   static targets = ["form", "input", "results", "clearButton"];
 
   search() {
-    clearTimeout(this.timeout)
-    this.timeout = setTimeout(() => {
-      this.formTarget.requestSubmit()
-    }, 300)
-  }  // Show results when input is focused
-
-  showResults() {
-    this.resultsTarget.classList.add("focused");
+    this.formTarget.requestSubmit()
   }
 
-  // Hide results after a slight delay when input loses focus
-  hideResults() {
-    setTimeout(() => {
-      this.resultsTarget.classList.remove("focused");
-    }, 200);
-  }
-
-  // Clear the input and update the UI
   clear() {
     this.inputTarget.value = "";
     this.toggleClearButton();
     this.search();
   }
 
-  // Toggle clear button visibility based on input value
   toggleClearButton() {
     if (this.inputTarget.value) {
-      this.clearButtonTarget.style.display = "inline";
+      this.clearButtonTarget.classList.add("block");
+      this.clearButtonTarget.classList.remove("hidden");
     } else {
-      this.clearButtonTarget.style.display = "none";
+      this.clearButtonTarget.classList.add("hidden");
+      this.clearButtonTarget.classList.remove("block");
     }
   }
 }
