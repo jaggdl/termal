77ce7ea2cd89224b6a2edd3bf17dbc1a4c6b808c
jaggdl <ja125garcia@gmail.com>
2025-03-23 21:44:30 -0600

    Move base64 meal img logic to meal model

--- app/jobs/process_meal_image_job.rb
+++ app/jobs/process_meal_image_job.rb
@@ -21,14 +21,7 @@ class ProcessMealImageJob < ApplicationJob
 
   def get_meal_data(meal)
     openai_service = OpenAiService.new
-
-    base64_image = if meal.image.attached?
-      process_image(meal.image)
-    else
-      nil
-    end
-
-    openai_service.analyze_meal_image(base64_image:, prompt: meal.prompt)
+    openai_service.analyze_meal_image(base64_image: meal.base64_image, prompt: meal.prompt)
   end
 
   def handle_error(user_meal, error_code)
@@ -42,38 +35,5 @@ class ProcessMealImageJob < ApplicationJob
       partial: "shared/flash",
       locals: { flash: { alert: error_message } }
     )
-  end
-
-  def process_image(meal_image)
-    image_data = meal_image.download
-
-    temp_file = Tempfile.new([ "meal_image", ".jpg" ], binmode: true)
-    temp_file.write(image_data)
-    temp_file.rewind
-
-    begin
-      resized_image = resize_and_convert_image(temp_file.path)
-      convert_to_base64_with_mime(resized_image)
-    rescue => e
-      Rails.logger.error("Failed to process image: #{e.message}")
-      Rails.logger.error("Error backtrace: #{e.backtrace.join("\n")}")
-      raise StandardError.new(:image_processing_error)
-    ensure
-      temp_file.close
-      temp_file.unlink # Ensure the temp file is deleted
-    end
-  end
-
-  def resize_and_convert_image(image_path)
-    ImageProcessing::Vips
-      .source(image_path)
-      .resize_to_limit(1000, 1000)
-      .convert("png")  # Converts to PNG
-      .call
-  end
-
-  def convert_to_base64_with_mime(image)
-    encoded_image = Base64.strict_encode64(File.read(image.path))
-    "data:image/png;base64,#{encoded_image}"
   end
 end
--- app/models/meal.rb
+++ app/models/meal.rb
@@ -10,10 +10,29 @@ class Meal < ApplicationRecord
   end
 
   def image_path
-    if image.attached?
-      ActiveStorage::Blob.service.send(:path_for, image.key)
-    else
-      nil
-    end
+    return nil unless image.attached?
+    ActiveStorage::Blob.service.send(:path_for, image.key)
+  end
+
+  def base64_image
+    return nil unless image.attached?
+
+    resized_image = resize_and_convert_image
+    convert_to_base64_with_mime(resized_image)
+  end
+
+  private
+
+  def resize_and_convert_image
+    ImageProcessing::Vips
+      .source(image_path)
+      .resize_to_limit(1000, 1000)
+      .convert("png")  # Converts to PNG
+      .call
+  end
+
+  def convert_to_base64_with_mime(image)
+    encoded_image = Base64.strict_encode64(File.read(image.path))
+    "data:image/png;base64,#{encoded_image}"
   end
 end
