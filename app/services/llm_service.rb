d8604e5fa87e74cdd42ebde3d441b1ce18c008d3
jaggdl <ja125garcia@gmail.com>
2025-03-23 18:50:47 -0600

    Change model to gpt 4o

--- app/services/llm_service.rb
+++ app/services/llm_service.rb
@@ -2,7 +2,7 @@ class LlmService
   def initialize
     @openai_api_key = GlobalSetting.get("openai_api_key")
     @anthropic_api_key = GlobalSetting.get("anthropic_api_key")
-    @model = "o1"
+    @model = "gpt-4o"
 
     unless @openai_api_key
       raise StandardError, "OpenAI API key is not set. Please set it in the global settings."
