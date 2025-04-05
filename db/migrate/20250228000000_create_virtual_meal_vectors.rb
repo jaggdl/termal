fd39b76facfb1f293a9e38356d6747501a52faf8
jaggdl <ja125garcia@gmail.com>
2025-04-05 12:59:04 -0600

    More chunks error BS

--- db/migrate/20250228000000_create_virtual_meal_vectors.rb
+++ db/migrate/20250228000000_create_virtual_meal_vectors.rb
@@ -1,7 +1,17 @@
 class CreateVirtualMealVectors < ActiveRecord::Migration[8.0]
   def up
-    # First check if the chunks table already exists and drop it if it does
-    execute "DROP TABLE IF EXISTS meal_vectors_chunks"
+    # Drop all related tables that might exist
+    tables = [
+      "meal_vectors",
+      "meal_vectors_chunks",
+      "meal_vectors_info",
+      "meal_vectors_rowids", 
+      "meal_vectors_vector_chunks00"
+    ]
+    
+    tables.each do |table|
+      execute "DROP TABLE IF EXISTS #{table}"
+    end
     
     # Then create the virtual table
     create_virtual_table :meal_vectors, :vec0, [
@@ -11,10 +21,17 @@ class CreateVirtualMealVectors < ActiveRecord::Migration[8.0]
   end
   
   def down
-    execute "DROP TABLE IF EXISTS meal_vectors"
-    execute "DROP TABLE IF EXISTS meal_vectors_chunks"
-    execute "DROP TABLE IF EXISTS meal_vectors_info"
-    execute "DROP TABLE IF EXISTS meal_vectors_rowids"
-    execute "DROP TABLE IF EXISTS meal_vectors_vector_chunks00"
+    # Drop all related tables
+    tables = [
+      "meal_vectors",
+      "meal_vectors_chunks",
+      "meal_vectors_info",
+      "meal_vectors_rowids", 
+      "meal_vectors_vector_chunks00"
+    ]
+    
+    tables.each do |table|
+      execute "DROP TABLE IF EXISTS #{table}"
+    end
   end
 end
