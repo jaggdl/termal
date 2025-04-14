b6e52aec310d852973c556e9064a5a0cdad17fcd
jaggdl <ja125garcia@gmail.com>
2025-04-13 23:18:05 -0600

    Complete dark mode

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -20,15 +20,15 @@ module ApplicationHelper
     base_classes = "transition-colors duration-200"
     active_classes = "font-bold text-xl text-black dark:text-dark-text"
     inactive_classes = "text-gray-600 dark:text-gray-400 hover:text-gray-800 dark:hover:text-gray-200"
-    
+
     "#{base_classes} #{is_active ? active_classes : inactive_classes}"
   end
-  
+
   def nav_icon_classes(is_active)
     base_classes = "h-6 w-6 transition-colors duration-200"
     active_classes = "text-sky-500 dark:text-dark-primary"
     inactive_classes = "text-gray-500 dark:text-gray-400"
-    
+
     "#{base_classes} #{is_active ? active_classes : inactive_classes}"
   end
 
--- db/migrate/20250228000000_create_virtual_meal_vectors.rb
+++ db/migrate/20250228000000_create_virtual_meal_vectors.rb
@@ -5,31 +5,31 @@ class CreateVirtualMealVectors < ActiveRecord::Migration[8.0]
       "meal_vectors",
       "meal_vectors_chunks",
       "meal_vectors_info",
-      "meal_vectors_rowids", 
+      "meal_vectors_rowids",
       "meal_vectors_vector_chunks00"
     ]
-    
+
     tables.each do |table|
       execute "DROP TABLE IF EXISTS #{table}"
     end
-    
+
     # Then create the virtual table
     create_virtual_table :meal_vectors, :vec0, [
       "meal_id integer primary key",
       "embedding float[1536] distance_metric=cosine"
     ]
   end
-  
+
   def down
     # Drop all related tables
     tables = [
       "meal_vectors",
       "meal_vectors_chunks",
       "meal_vectors_info",
-      "meal_vectors_rowids", 
+      "meal_vectors_rowids",
       "meal_vectors_vector_chunks00"
     ]
-    
+
     tables.each do |table|
       execute "DROP TABLE IF EXISTS #{table}"
     end
