bbb342e1f112f8b059c5bb10cca19e117b4324b8
jaggdl <ja125garcia@gmail.com>
2025-04-14 23:50:35 -0600

    Add prose tailwind plugin

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -26,7 +26,7 @@ module.exports = {
   },
   plugins: [
     // require('@tailwindcss/forms'),
-    // require('@tailwindcss/typography'),
-    // require('@tailwindcss/container-queries'),
+    require("@tailwindcss/typography"),
+    // require("@tailwindcss/container-queries"),
   ],
 };
