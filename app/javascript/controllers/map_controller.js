0fc1f8e57ef8b4e6494db2ff2a3ea06f8774f2af
jaggdl <ja125garcia@gmail.com>
2025-10-15 14:09:30 -0600

    Add location map JS controller

---
+++ app/javascript/controllers/map_controller.js
@@ -0,0 +1,87 @@
+import { Controller } from "@hotwired/stimulus";
+
+export default class extends Controller {
+  static targets = ["container"];
+  static values = {
+    latitude: Number,
+    longitude: Number,
+  };
+
+  connect() {
+    this.loadLeaflet();
+  }
+
+  disconnect() {
+    if (this.map) {
+      this.map.remove();
+    }
+    if (this.observer) {
+      this.observer.disconnect();
+    }
+  }
+
+  async loadLeaflet() {
+    if (!window.L) {
+      const link = document.createElement("link");
+      link.rel = "stylesheet";
+      link.href = "https://unpkg.com/leaflet@1.9.4/dist/leaflet.css";
+      document.head.appendChild(link);
+
+      const script = document.createElement("script");
+      script.src = "https://unpkg.com/leaflet@1.9.4/dist/leaflet.js";
+      await new Promise((resolve) => {
+        script.onload = resolve;
+        document.head.appendChild(script);
+      });
+    }
+
+    this.initializeMap();
+    this.observer = new MutationObserver(() => this.updateMapTheme());
+    this.observer.observe(document.documentElement, {
+      attributes: true,
+      attributeFilter: ["class"],
+    });
+  }
+
+  initializeMap() {
+    this.map = L.map(this.containerTarget, {
+      zoomControl: true,
+      scrollWheelZoom: false,
+    }).setView([this.latitudeValue, this.longitudeValue], 15);
+
+    this.updateMapTheme();
+
+    L.marker([this.latitudeValue, this.longitudeValue]).addTo(this.map);
+  }
+
+  updateMapTheme() {
+    if (!this.map) return;
+
+    if (this.tileLayer) {
+      this.map.removeLayer(this.tileLayer);
+    }
+
+    const isDark = document.documentElement.classList.contains("dark");
+
+    if (isDark) {
+      this.tileLayer = L.tileLayer(
+        "https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png",
+        {
+          attribution:
+            '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+          subdomains: "abcd",
+          maxZoom: 20,
+        },
+      ).addTo(this.map);
+    } else {
+      this.tileLayer = L.tileLayer(
+        "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+        {
+          attribution:
+            '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+          maxZoom: 19,
+        },
+      ).addTo(this.map);
+    }
+  }
+}
