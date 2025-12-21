7c20a6b2152c5c875b1aea7b8286b119c7193bda
jaggdl <ja125garcia@gmail.com>
2025-12-21 15:02:39 -0700

    Show other providers api keys if set

--- app/controllers/user_profiles_controller.rb
+++ app/controllers/user_profiles_controller.rb
@@ -4,7 +4,7 @@ class UserProfilesController < ApplicationController
   def show
     @profile = Current.user_profile || UserProfile.new
     @profile.timezone ||= cookies[:timezone]
-    @global_settings = GlobalSetting.where(name: "openai_api_key") if Current.user.is_admin?
+    @global_settings = GlobalSetting.where(name: %w[openai_api_key anthropic_api_key gemini_api_key]) if Current.user.is_admin?
   end
 
   def update
